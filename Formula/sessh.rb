a0cb0a2b0f8cf34096e905ec3b0e7cfd40fd68c6
Travis CI <travis@travis-ci.org>
2019-10-02 10:30:23 +0000

    Publish v1.0.0-beta.28

--- Formula/sessh.rb
+++ Formula/sessh.rb
@@ -1,7 +1,7 @@
 class Sessh < Formula
     homepage "https://github.com/QtonSolutions/sessh"
-    url "https://github.com/QtonSolutions/sessh/releases/download/v1.0.0-beta.26/sessh-v1.0.0-beta.26-osx.gz"
-    sha256 "1467fbd3684360a0f9352f4e695ff93d550368196c92529a3a435b185808e811"
+    url "https://github.com/QtonSolutions/sessh/releases/download/v1.0.0-beta.28/sessh-v1.0.0-beta.28-osx.gz"
+    sha256 "056a74f381fb25d12ff7ec6fb1967d206b57b7865e56f2277338aba02a38ce39"
 
     bottle :unneeded
 
